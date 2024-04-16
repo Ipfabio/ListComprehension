3f08aeb402af3ee875c26764f591dc176f44e2db
Fabio <fabioperrone95@hotmail.com>
2024-04-16 19:18:59 -0300

    Ejemplo de List Comprehension con  listas anidadas

---
+++ nested_list_comprehension.py
@@ -0,0 +1,24 @@
+# Building a 3D list
+import pprint
+
+printer = pprint.PrettyPrinter()
+
+lst = []
+
+for a in range(5):
+    l1 = []
+    for b in range(5):
+        l2 = []
+        for num in range(5):
+            l2.append(num)
+        l1.append(l2)
+
+    lst.append(l1)
+
+# printer.pprint(lst)
+
+# ----
+
+lst_v2 = [[[num for num in range(5)] for _ in range(5)] for _ in range(5)]
+
+printer.pprint(lst_v2)
